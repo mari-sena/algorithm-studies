034a9e9607d740e3fba86975eef1cbed1dd8f1bb
Mariana Sena <35273542+mari-sena@users.noreply.github.com>
2026-04-18 17:50:43 -0300

    Implement twoSum function to find indices of target

---
+++ two-sum-basic.ts
@@ -0,0 +1,8 @@
+function twoSum(nums: number[], target: number): number[] {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] + nums[i] == target)
+                return [i, j];
+        }
+    }
+};
